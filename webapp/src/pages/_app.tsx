f95ecb4c91d42a9b60d57217cf70beb47a1a5f52
dork <62394594+dorian-K@users.noreply.github.com>
2024-03-17 21:10:07 +0100

    display data in a graph

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { BackendProvider } from "@/components/BackendProvider";
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <BackendProvider>
-            <Component {...pageProps} />
-        </BackendProvider>
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>RWTF</title>
+            </Head>
+            <BackendProvider>
+                <Component {...pageProps} />
+            </BackendProvider>
+        </>
     );
 }
--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { Backend, GymResponse } from "@/api/Backend";
 import { useBackendContext } from "@/components/BackendProvider";
+import { ApexOptions } from "apexcharts";
+import React from "react";
 import { useEffect, useState } from "react";
+
+const ReactApexChart = React.lazy(() => import("react-apexcharts"));
 
 export default function Home() {
     const [gym, setGym] = useState<GymResponse["data"]>([]);
@@ -12,18 +16,87 @@ export default function Home() {
         });
     }, [api]);
 
+    if (gym.length === 0) {
+        return <div>Loading...</div>;
+    }
+
+    let data = gym.map((g) => ({
+        ...g,
+        created_at: Date.parse(g.created_at),
+    }));
+    data = data.sort((a, b) => a.created_at - b.created_at);
+    let maxX = data[data.length - 1].created_at + 1000 * 60 * 60 * 6; // 6 hours
+    let range = 1000 * 60 * 60 * 24; // 1 day
+
+    const options: ApexOptions = {
+        yaxis: {
+            min: 0,
+            max: (max) => Math.max(150, Math.ceil(max / 10) * 10),
+            decimalsInFloat: 0,
+            tickAmount: 6,
+        },
+        xaxis: {
+            type: "datetime",
+            min: maxX - range,
+            max: maxX,
+        },
+        chart: {
+            id: "gym",
+            animations: {
+                enabled: false,
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            curve: "smooth",
+        },
+        title: {
+            text: "RWTH Gym Auslastung",
+            align: "left",
+        },
+        theme: {
+            mode: "dark",
+        },
+        tooltip: {
+            x: {
+                format: "dd.MM.yyyy HH:mm",
+            },
+        },
+        grid: {
+            borderColor: "#636363",
+            xaxis: {
+                lines: {
+                    show: true,
+                },
+            },
+        },
+    };
+
+    const series: ApexAxisChartSeries = [
+        {
+            name: "Auslastung",
+            data: data.map((g) => ({
+                x: g.created_at,
+                y: g.auslastung,
+            })),
+        },
+    ];
+
     return (
-        <div>
-            <h1>RWTH Gym Auslastung</h1>
-            <ul>
-                {gym.map((entry) => {
-                    return (
-                        <li key={entry.created_at}>
-                            {entry.auslastung} at {entry.created_at}
-                        </li>
-                    );
-                })}
-            </ul>
+        <div className="container">
+            <div className="card mt-3">
+                <div className="card-body">
+                    <ReactApexChart
+                        type="area"
+                        width={"100%"}
+                        height={500}
+                        options={options}
+                        series={series}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
