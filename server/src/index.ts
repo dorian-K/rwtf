bcb1d9936c9778dd02f3019f43e09af8646ccf7a
dork <62394594+dorian-K@users.noreply.github.com>
2024-05-14 21:21:56 +0200

    Add dayoffset query parameter

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,24 +33,32 @@ async function gymCrawl() {
         });
 }
 app.get("/api/v1/gym", async (req, res) => {
+    // get offset from query
+    let dayoffset = req.query.dayoffset ? parseInt(req.query.dayoffset as string) : 0;
+
+    if (dayoffset < 0 || dayoffset > 3 || isNaN(dayoffset)) {
+        res.status(400).send('{error: true, msg: "Invalid dayoffset"}');
+        return;
+    }
+
     let conn;
     try {
         conn = await getConnection();
         let weeks = [];
-        for(let i = 0; i <= 3; i++){
+        for (let i = 0; i <= 3; i++) {
             const startDate = new Date();
-            startDate.setDate(startDate.getDate() - i * 7);
-            startDate.setHours(4, 0, 0, 0);
+            startDate.setDate(startDate.getDate() - i * 7 + dayoffset);
+            startDate.setHours(6, 0, 0, 0);
 
             const endDate = new Date();
-            endDate.setDate(endDate.getDate() - i * 7);
+            endDate.setDate(endDate.getDate() - i * 7 + dayoffset);
             endDate.setHours(23, 59, 59, 999);
 
             const rows = await conn.query(
                 "SELECT auslastung, created_at FROM rwth_gym WHERE created_at >= ? AND created_at <= ? LIMIT 500",
                 [startDate, endDate]
             );
-            
+
             const sanitized = rows.map((row: any) => {
                 return {
                     auslastung: row.auslastung,
@@ -60,7 +68,7 @@ app.get("/api/v1/gym", async (req, res) => {
             weeks.push(sanitized);
         }
 
-        res.json({ data_today: weeks[0], data_historic: weeks.slice(1) });
+        res.json({ data_today: weeks[0], data_historic: weeks.slice(1), dayoffset: dayoffset });
     } catch (err) {
         console.error(err);
         res.status(500).send("{error: true}");
@@ -73,13 +81,31 @@ app.get("/api", (req, res) => {
     res.send("Hello World!");
 });
 
-// init database
-getConnection()
-    .then((conn) => {
-        return conn.query(
+const database_init = async () => {
+    let conn;
+    try {
+        conn = await getConnection();
+        await conn.query(
             "CREATE TABLE IF NOT EXISTS rwth_gym (id INT AUTO_INCREMENT PRIMARY KEY, auslastung INT NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
         );
-    })
+        // index for created_at
+        await conn.query("CREATE INDEX IF NOT EXISTS idx_created_at ON rwth_gym (created_at)");
+        // other optimizations
+        await conn.query("OPTIMIZE TABLE rwth_gym");
+        await conn.query("ANALYZE TABLE rwth_gym");
+        console.log("Database initialized");
+
+        if (conn) conn.end();
+    } catch (err) {
+        console.error(err);
+        if (conn) conn.end();
+        throw err;
+    }
+};
+
+// init database
+database_init()
+    .then(database_init)
     .then(() => {
         setInterval(gymCrawl, 1000 * 60 * 5); // 5 minutes
         gymCrawl();
